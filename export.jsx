170dea5b8d9a700ddd058ed29ff439e808640210
pravdomil <pravdomil.toman@gmail.com>
2017-03-09 18:09:34 +0100

    first

---
+++ export.jsx
@@ -0,0 +1,49 @@
+main()
+
+function main() {
+  // no documents open
+  if(!app.activeDocument) { return }
+	
+  const doc = app.activeDocument
+  // document not saved
+  if(!doc.fullName) { return }
+  
+  group(doc)
+  html_export(doc)
+  ungroup(doc)
+}
+
+function group(doc) {
+  for(var i = 0; i < doc.pages.length; i++) {
+    const page = doc.pages[i]
+    const items = page.allPageItems
+    const group = []
+    
+    for(var i = 0; i < items.length; i++) {
+      if(items[i].parent.constructor == Spread) {
+        group.push(items[i])
+      }
+    }
+    
+    page.groups.add(group)
+  }
+}
+
+function ungroup(doc) {
+  for(var i = 0; i < doc.pages.length; i++) {
+    const page = doc.pages[i]
+    const items = doc.allPageItems
+    page.groups.firstItem().ungroup()
+  }
+}
+
+function html_export(doc) {
+  const basename = doc.fullName.displayName
+	const basenameWithoutExt = basename.substring(0, basename.lastIndexOf('.'))
+  
+  const root = new Folder(doc.filePath + '/html')
+  root.create()
+  
+  const showDialog = true
+	doc.exportFile(ExportFormat.HTML, new File(root + '/' + basenameWithoutExt + '.html'), showDialog)
+}
