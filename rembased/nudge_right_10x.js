7206dc4cb5b9b559507900b01578808aa6c7c704
pravdomil <pravdomil.toman@gmail.com>
2017-05-02 16:17:25 +0200

    nudge x10

---
+++ rembased/nudge_right_10x.js
@@ -0,0 +1,2 @@
+#include "lib.js"
+app.doScript(nudge, ScriptLanguage.JAVASCRIPT, [.5 * 8, 0], UndoModes.ENTIRE_SCRIPT, "Nudge")
---
+++ rembased/nudge_up_10x.js
@@ -0,0 +1,2 @@
+#include "lib.js"
+app.doScript(nudge, ScriptLanguage.JAVASCRIPT, [0, -.5 * 8], UndoModes.ENTIRE_SCRIPT, "Nudge")
