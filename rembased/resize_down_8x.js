4692a618b59cde65e8da57e8cb2578f42e249921
pravdomil <pravdomil.toman@gmail.com>
2017-05-10 21:18:15 +0200

    resize 8x

---
+++ rembased/resize_down_8x.js
@@ -0,0 +1,2 @@
+#include "lib.js"
+app.doScript(resize, ScriptLanguage.JAVASCRIPT, [0, .5 * 8], UndoModes.ENTIRE_SCRIPT, "Resize")
---
+++ rembased/resize_right_8x.js
@@ -0,0 +1,2 @@
+#include "lib.js"
+app.doScript(resize, ScriptLanguage.JAVASCRIPT, [.5 * 8, 0], UndoModes.ENTIRE_SCRIPT, "Resize")
