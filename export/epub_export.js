ea6957524dc4edeb1705b2acfb3a0337f3ded456
pravdomil <pravdomil.toman@gmail.com>
2017-05-11 18:48:23 +0200

    fix

--- export/epub_export.js
+++ export/epub_export.js
@@ -52,7 +52,7 @@ function conformFile(file, title, filename) {
   file.open('e')
   var content = file.read()
   content = content.replace('<title>' + filename + '</title>', '<title>' + title + '</title> <meta name="viewport" content="width=device-width" /> <script>window.top.isPreviewFile = function() { return {} }</script> <script>window.top.shouldNavigate = function() { return true }</script><script>window.top.onFrameDOMLoaded = function() { return true }</script> <script> function press(innerText) { var butons = document.querySelectorAll(\'._idGenButton\'); for(var i = 0; i < butons.length; i++) { var button = butons[i]; var match = button.textContent.replace(/\\s/g, \'\') == innerText; if(match) { var evt = document.createEvent("MouseEvents"); evt.initEvent("mouseup", true, true); button.dispatchEvent(evt); console.log(\'fired\'); return; } } } </script>')
-  content = content.replace('style="', 'style="margin: auto position: relative ')
+  content = content.replace('style="', 'style="margin: auto; position: relative;')
   file.seek(0)
   file.write(content)
   file.close()
