c43214fa7f0ae965a161a46f4552fe07d56607ed
pravdomil <pravdomil.toman@gmail.com>
2017-05-20 18:01:58 +0200

    open_page

--- export/lib/support.js
+++ export/lib/support.js
@@ -1,11 +1,7 @@
 function open_page(file, currentPage) {
-  try {
-    var filepath = file.fullName.replace(/\.html$/, (currentPage - 1 ? '-' + currentPage - 1 : '') + '.html')
-    var file = new File(filepath)
-    var openLocation = 'tell application "System Events" to open location "file://' + file.fsName + '"'
-    app.doScript(openLocation, ScriptLanguage.applescriptLanguage)
-  }
-  catch (e) {
-    
-  }
+  var suffix = (currentPage == 1) ? '' : '-' + (currentPage - 1)
+  var filepath = file.fullName.replace(/\.html$/, suffix + '.html')
+  var file = new File(filepath)
+  var openLocation = 'tell application "System Events" to open location "file://' + file.fsName + '"'
+  app.doScript(openLocation, ScriptLanguage.applescriptLanguage)
 }
