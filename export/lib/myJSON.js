18021d1a9dba24581e2f1fb52e4eec343fb7488b
pravdomil <pravdomil.toman@gmail.com>
2017-05-20 15:39:24 +0200

    fix myJSON

--- export/lib/myJSON.js
+++ export/lib/myJSON.js
@@ -2,7 +2,7 @@ function myJSONparse(string) {
   if(typeof string != 'string') { return {} }
   
   var obj = {}
-  var objects = string.split('++')
+  var objects = string.split('++\n')
   for(var i = 0; i < objects.length; i++) {
     var keyvalue = objects[i].split('+')
     if(keyvalue[0]) {
@@ -18,7 +18,7 @@ function myJSONstringify(obj) {
   for(var key in obj) {
     var value = obj[key]
     if(!value) { value = '' }
-    string += key + '+' + value + '++'
+    string += key + '+' + value + '++\n'
   }
   
   return string
