d5c2cd7798dcc7ef8f3123a89cc8457601d85d9f
pravdomil <pravdomil.toman@gmail.com>
2017-05-20 18:59:25 +0200

    refactor

--- export/lib/optimalize.js
+++ export/lib/optimalize.js
@@ -9,16 +9,16 @@ function optimalize_html(doc, file, opt) {
   if(!opt.currentPage) {
     for(var i = 1; i < doc.pages.length; i++) {
       var f = new File(file.parent + '/' + file.nameWithoutExt() + '-' + i + '.html')
-      optimalize_file(doc, f, opt, file)
+      optimalize_file(doc, file, opt, f)
     }
   }
 }
 
-function optimalize_file(doc, file, opt, masterFile) {
-  file.lineFeed = 'Unix'
-  file.open('e')
+function optimalize_file(doc, file, opt, current) {
+  current.lineFeed = 'Unix'
+  current.open('e')
   
-  var content = file.read()
+  var content = current.read()
   var head = '<title>' + doc.fullName.nameWithoutExt() + '</title>' +
     '\t\t<meta name="viewport" content="width=device-width" />\n' +
     '\t\t<script>window.top.isPreviewFile = function() { return {} }</script>\n' +
@@ -26,14 +26,14 @@ function optimalize_file(doc, file, opt, masterFile) {
     '\t\t<script>window.top.onFrameDOMLoaded = function() { return true }</script>\n' +
     '\t\t<script>function press(innerText) { var butons = document.querySelectorAll(\'._idGenButton\'); for(var i = 0; i < butons.length; i++) { var button = butons[i]; var match = button.textContent.replace(/\\s/g, \'\') == innerText; if(match) { var evt = document.createEvent("MouseEvents"); evt.initEvent("mouseup", true, true); button.dispatchEvent(evt); console.log(\'fired\'); return; } } } </script>\n'
   
-  content = content.replace('<title>' + file.nameWithoutExt() + '</title>', head)
+  content = content.replace('<title>' + current.nameWithoutExt() + '</title>', head)
   content = content.replace('style="', 'style="margin: auto; position: relative;')
   
   if(!opt.keepFontFiles) {
-    content = content.replace('<script src="' + masterFile.nameWithoutExt() + '-web-resources/script/FontData.js" type="text/javascript"></script>', '')
+    content = content.replace('<script src="' + file.nameWithoutExt() + '-web-resources/script/FontData.js" type="text/javascript"></script>', '')
   }
   
-  file.seek(0)
-  file.write(content)
-  file.close()
+  current.seek(0)
+  current.write(content)
+  current.close()
 }
