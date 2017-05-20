5c4c90f5e946594fef0d10f94575d58b95bcb7d0
pravdomil <pravdomil.toman@gmail.com>
2017-05-20 15:13:16 +0200

    myJSON

---
+++ export/lib/myJSON.js
@@ -0,0 +1,23 @@
+function myJSONparse(string) {
+  if(typeof string != 'string') { return {} }
+  
+  var obj = {}
+  var objects = string.split('++')
+  for(var i = 0; i < objects.length; i++) {
+    var keyvalue = objects[i].split('+')
+    if(keyvalue[0]) {
+      obj[keyvalue[0]] = keyvalue[1]
+    }
+  }
+  
+  return obj
+}
+
+function myJSONstringify(obj) {
+  var string = ''
+  for(var key in obj) {
+    string += key + '+' + obj[key] + '++'
+  }
+  
+  return string
+}
