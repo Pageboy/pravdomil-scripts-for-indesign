ca36878d9b985750e45ca188c12838519544023d
pravdomil <pravdomil.toman@gmail.com>
2017-05-28 18:48:21 +0200

    hmmm?

--- export/lib/support.js
+++ export/lib/support.js
@@ -2,8 +2,10 @@ function open_page(file, currentPage) {
   var suffix = (currentPage == 1) ? '' : '-' + (currentPage - 1)
   var file = new File(file.parent + '/' + file.nameWithoutExt() + suffix + '.html')
   
-  var openLocation = 'tell application "System Events" to open location "file://' + file.fsName + '"'
-  app.doScript(openLocation, ScriptLanguage.applescriptLanguage)
+  if(file.exists) {
+    var openLocation = 'tell application "System Events" to open location "file://' + file.fsName + '"'
+    app.doScript(openLocation, ScriptLanguage.applescriptLanguage)
+  }
 }
 
 File.prototype.nameWithoutExt = function() {
