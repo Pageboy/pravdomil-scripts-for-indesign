b8797d2576e5b95022aa137cceccaf2b906a017e
pravdomil <pravdomil.toman@gmail.com>
2017-09-04 23:09:03 +0200

    let

--- export/html_export.ts
+++ export/html_export.ts
@@ -9,18 +9,18 @@ app.doScript(epub_export, ScriptLanguage.JAVASCRIPT, [], UndoModes.ENTIRE_SCRIPT
 function epub_export() {
   if(!app.documents.length) { return }
   
-  var doc = app.activeDocument;
+  let doc = app.activeDocument;
   
-  var opt = options_dialog(doc);
+  let opt = options_dialog(doc);
   if(!opt) { return }
   
-  var file = new File(opt.outputFile);
+  let file = new File(opt.outputFile);
   if(opt.versioning) {
     file = new File(file.parent + '/' + versionString() + '/' + file.displayName);
     if(!file.parent.exists) { file.parent.create() }
   }
   
-  var exportPref = doc.htmlFXLExportPreferences;
+  let exportPref = doc.htmlFXLExportPreferences;
   if(opt.onlyCurrentPage) {
     exportPref.epubPageRangeFormat = PageRangeFormat.EXPORT_PAGE_RANGE;
     exportPref.epubPageRange = app.activeWindow.activePage.name
@@ -31,15 +31,15 @@ function epub_export() {
   
   doc.exportFile(ExportFormat.HTMLFXL, file, true);
   
-  var files = [file];
-  for(var i = 1; i < doc.pages.length; i++) {
-    var f = new File(file.parent + '/' + file.nameWithoutExt() + '-' + i + '.html');
+  let files = [file];
+  for(let i = 1; i < doc.pages.length; i++) {
+    let f = new File(file.parent + '/' + file.nameWithoutExt() + '-' + i + '.html');
     files.push(f)
   }
   
   optimalize_html(doc, files, opt);
   
-  var currentFile = files[(app.activeWindow.activePage.name - 1) | 0];
+  let currentFile = files[(app.activeWindow.activePage.name - 1) | 0];
   if(opt.onlyCurrentPage || opt.mergePages) { currentFile = file[0] }
   
   open_file(file, currentFile)
