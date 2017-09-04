bd78d9b9c78bcffd259103686a98fd1ff0078afc
pravdomil <pravdomil.toman@gmail.com>
2017-09-04 23:07:38 +0200

    not needed?

--- states/lib.ts
+++ states/lib.ts
@@ -1,5 +1,3 @@
-/// <reference path="../node_modules/types-for-adobe/InDesign/2015.3/index.d.ts" />
-
 function switchObjectState(obj: MultiStateObject, direction: number) {
   obj.activeStateIndex = modulo(obj.activeStateIndex + direction, obj.states.length);
   obj.select();
