0585132767a8e140938c7d496c1864618976fbe5
pravdomil <pravdomil.toman@gmail.com>
2017-09-04 23:08:52 +0200

    semicolon

--- export/html_export.ts
+++ export/html_export.ts
@@ -4,42 +4,42 @@
 //@include "./lib/support.js"
 //@include "./lib/optimalize.js"
 
-app.doScript(epub_export, ScriptLanguage.JAVASCRIPT, [], UndoModes.ENTIRE_SCRIPT, "HTML Export")
+app.doScript(epub_export, ScriptLanguage.JAVASCRIPT, [], UndoModes.ENTIRE_SCRIPT, "HTML Export");
 
 function epub_export() {
   if(!app.documents.length) { return }
   
-  var doc = app.activeDocument
+  var doc = app.activeDocument;
   
-  var opt = options_dialog(doc)
+  var opt = options_dialog(doc);
   if(!opt) { return }
   
-  var file = new File(opt.outputFile)
+  var file = new File(opt.outputFile);
   if(opt.versioning) {
-    file = new File(file.parent + '/' + versionString() + '/' + file.displayName)
+    file = new File(file.parent + '/' + versionString() + '/' + file.displayName);
     if(!file.parent.exists) { file.parent.create() }
   }
   
-  var exportPref = doc.htmlFXLExportPreferences
+  var exportPref = doc.htmlFXLExportPreferences;
   if(opt.onlyCurrentPage) {
-    exportPref.epubPageRangeFormat = PageRangeFormat.EXPORT_PAGE_RANGE
+    exportPref.epubPageRangeFormat = PageRangeFormat.EXPORT_PAGE_RANGE;
     exportPref.epubPageRange = app.activeWindow.activePage.name
   }
   else {
     exportPref.epubPageRangeFormat = PageRangeFormat.EXPORT_ALL_PAGES
   }
   
-  doc.exportFile(ExportFormat.HTMLFXL, file, true)
+  doc.exportFile(ExportFormat.HTMLFXL, file, true);
   
-  var files = [file]
+  var files = [file];
   for(var i = 1; i < doc.pages.length; i++) {
-    var f = new File(file.parent + '/' + file.nameWithoutExt() + '-' + i + '.html')
+    var f = new File(file.parent + '/' + file.nameWithoutExt() + '-' + i + '.html');
     files.push(f)
   }
   
-  optimalize_html(doc, files, opt)
+  optimalize_html(doc, files, opt);
   
-  var currentFile = files[(app.activeWindow.activePage.name - 1) | 0]
+  var currentFile = files[(app.activeWindow.activePage.name - 1) | 0];
   if(opt.onlyCurrentPage || opt.mergePages) { currentFile = file[0] }
   
   open_file(file, currentFile)
