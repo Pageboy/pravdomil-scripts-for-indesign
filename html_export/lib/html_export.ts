809cacc09741a8f3923c4863289c56bdc756edec
pravdomil <pravdomil.toman@gmail.com>
2017-09-05 16:29:35 +0200

    move

---
+++ html_export/lib/html_export.ts
@@ -0,0 +1,56 @@
+interface PravdomilExportOptionsSettings {
+  onlyCurrentPage?: boolean;
+
+  mergePages?: boolean;
+
+  keepFontFiles?: boolean;
+  versioning?: boolean;
+
+  outputFile?: string;
+}
+
+interface PravdomilExportOptions {
+  document: Document;
+  settings: PravdomilExportOptionsSettings;
+}
+
+function pravdomilExport(paramOpt?: Partial<PravdomilExportOptions>) {
+  let opt: PravdomilExportOptions = paramOpt as PravdomilExportOptions;
+
+  if(!opt.document) {
+    if(!app.documents.length) { return }
+    opt.document = app.activeDocument
+  }
+  if(!opt.settings) {
+    pravdomilExportOptionsDialog(opt);
+  }
+
+  let file = new File(opt.settings.outputFile);
+  if(opt.settings.versioning) {
+    file = new File(file.parent + "/" + versionString() + "/" + file.displayName);
+    if(!file.parent.exists) { file.parent.create() }
+  }
+  
+  let exportPref = opt.document.htmlFXLExportPreferences;
+  if(opt.settings.onlyCurrentPage) {
+    exportPref.epubPageRangeFormat = PageRangeFormat.EXPORT_PAGE_RANGE;
+    exportPref.epubPageRange = app.activeWindow.activePage.name
+  }
+  else {
+    exportPref.epubPageRangeFormat = PageRangeFormat.EXPORT_ALL_PAGES
+  }
+  
+  opt.document.exportFile(ExportFormat.HTMLFXL, file, true);
+  
+  let files = [file];
+  for(let i = 1; i < opt.document.pages.length; i++) {
+    let f = new File(file.parent + "/" + file.nameWithoutExt() + "-" + i + ".html");
+    files.push(f)
+  }
+  
+  optimize_html(opt.document, files, opt);
+  
+  let currentFile = files[(app.activeWindow.activePage.name - 1) | 0];
+  
+  openFile(file);
+}
