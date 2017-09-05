c0a9bc9eb2007eb914b6e8b069eee60f765c4d76
pravdomil <pravdomil.toman@gmail.com>
2017-09-05 21:41:09 +0200

    bodyFilters

--- html_export/lib/html_export.ts
+++ html_export/lib/html_export.ts
@@ -14,6 +14,7 @@ interface PravdomilExportOptions {
   file: File;
   files: File[];
   headFilters: ((opt: PravdomilExportOptions, i: number, head: string) => string)[];
+  bodyFilters: ((opt: PravdomilExportOptions, i: number, body: string) => string)[];
 }
 
 function pravdomilHTMLExport(options?: PravdomilExportOptions) {
@@ -35,6 +36,7 @@ function pravdomilHTMLExport(options?: PravdomilExportOptions) {
       file: new File(),
       files: [],
       headFilters: [],
+      bodyFilters: [],
     }
   }
   
