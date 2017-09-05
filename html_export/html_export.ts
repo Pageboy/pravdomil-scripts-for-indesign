2270b21538dd685dd97aa86f4ccbda6250db94f9
pravdomil <pravdomil.toman@gmail.com>
2017-09-05 16:30:24 +0200

    rename

--- html_export/html_export.ts
+++ html_export/html_export.ts
@@ -2,7 +2,7 @@
 //@include "./lib/support.js"
 
 //@include "./lib/html_export.js"
-//@include "./lib/options.js"
+//@include "./lib/settings.js"
 //@include "./lib/optimize.js"
 
 app.doScript(pravdomilHTMLExport, ScriptLanguage.JAVASCRIPT, [], UndoModes.ENTIRE_SCRIPT, "Pravdomil HTML Export");
