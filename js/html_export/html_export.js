7f0ea66e2196f30ba9ef08b18c90e3f7d3c87af9
pravdomil <pravdomil.toman@gmail.com>
2017-11-19 16:33:08 +0100

    lint

--- js/html_export/html_export.js
+++ js/html_export/html_export.js
@@ -1,14 +1,14 @@
 "use strict";
-//@include "./lib/prototype.js"
-//@include "./lib/support.js"
-//@include "./lib/html_export.js"
-//@include "./lib/settings.js"
-//@include "./lib/versioning.js"
-//@include "./lib/export.js"
-//@include "./lib/filter.js"
-//@include "./lib/keep_font.js"
-//@include "./lib/rembased.js"
-//@include "./lib/base_filters.js"
+// @include "./lib/prototype.js"
+// @include "./lib/support.js"
+// @include "./lib/html_export.js"
+// @include "./lib/settings.js"
+// @include "./lib/versioning.js"
+// @include "./lib/export.js"
+// @include "./lib/filter.js"
+// @include "./lib/keep_font.js"
+// @include "./lib/rembased.js"
+// @include "./lib/base_filters.js"
 app.doScript(function () {
     pravdomilHTMLExport();
 }, ScriptLanguage.JAVASCRIPT, [], UndoModes.ENTIRE_SCRIPT, "Pravdomil HTML Export");
